d64d8e3f44c062849d4c8065bdf61bf2bd4dc8a3
Devang_Randeria <devangranderia9@gmail.com>
2025-09-05 11:55:23 +0000

    initial changes

--- api/api.js
+++ api/api.js
@@ -1,3 +1,3 @@
-export let baseUrl = "http://localhost:3000"
+export let baseUrl = "https://didactic-spoon-pj9w7xgprp59c77w5-3000.app.github.dev"
 export let packageCards = `${baseUrl}/packageCards`
 export let contactQueries = `${baseUrl}/Contact-Questions`
