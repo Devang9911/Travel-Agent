eb824c5cc43b874f3bbe41f966c44fb99ae43bcf
Devang_Randeria <devangranderia9@gmail.com>
2025-09-05 10:01:00 +0000

    Initial commit

--- api/api.js
+++ api/api.js
@@ -1,3 +1,3 @@
-// export let baseUrl = "http://localhost:3000"
-export let packageCards = `https://68ba99e984055bce63ef944d.mockapi.io/packageCards`
-export let contactQueries = `https://68ba99e984055bce63ef944d.mockapi.io/Contact-Questions`
+export let baseUrl = "http://localhost:3000"
+export let packageCards = `${baseUrl}/packageCards`
+export let contactQueries = `${baseUrl}/Contact-Questions`
